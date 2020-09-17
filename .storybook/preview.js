fbaf635ae474eb7798686a96e76a1378afc16734
Peter Holló <lordpejdar@gmail.com>
2020-09-17 20:03:21 +0200

    Provide more viewports in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,10 @@
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+
+import "../src/styles/index.css";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import "./styles/index.css";
 import Dialog from "./components/Dialog";
 import tf from "./components/TextField";
 import btn from "./components/Button";
