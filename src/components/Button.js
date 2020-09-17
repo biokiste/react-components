ad85c087a72c5e12e50c4024540dca6c01333bf4
Peter Holló <lordpejdar@gmail.com>
2020-09-17 14:29:53 +0200

    Provide basic styling for Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,39 @@
 import React from "react";
 
+function getDefaultStyles(color, disabled) {
+  if (disabled) {
+    color = "gray";
+  }
+  const common =
+    "font-bold uppercase text-base px-8 py-4 rounded outline-none focus:outline-none";
+  const textColor = disabled ? `text-${color}-500` : "text-white";
+  const bgColor = disabled
+    ? `bg-${color}-200`
+    : `bg-${color}-500 active:bg-${color}-600 hover:bg-${color}-400 focus:bg-${color}-400`;
+  const extensions = disabled ? "cursor-not-allowed" : "";
+  return `${common} ${textColor} ${bgColor} ${extensions}`.trimRight();
+}
+
 function Button(props) {
-  const { onClick, children, ...otherProps } = props;
+  const {
+    onClick,
+    children,
+    classNameExt = "",
+    color = "blue",
+    disabled = false,
+    ...otherProps
+  } = props;
   return (
     <button
       type="button"
       onClick={onClick}
       data-testid="test-button"
+      className={`${getDefaultStyles(
+        color,
+        disabled
+      )} ${classNameExt}`.trimRight()}
+      style={{ transition: "all .15s ease" }}
+      disabled={disabled}
       {...otherProps}
     >
       {children}
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -3,6 +3,14 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { Button } from "..";
 
-storiesOf("Button", module).add("default", () => {
-  return <Button onClick={action("Button clicked!")}>Click me!</Button>;
-});
+storiesOf("Button", module)
+  .add("default", () => {
+    return <Button onClick={action("Button clicked!")}>Click</Button>;
+  })
+  .add("disabled", () => {
+    return (
+      <Button onClick={action("Button clicked!")} disabled>
+        Disabled
+      </Button>
+    );
+  });
